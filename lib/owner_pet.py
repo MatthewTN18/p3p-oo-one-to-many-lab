d153b0365b9968242ff88423ec9994d9a48cd268
MatthewTN18 <matthewtndirangu@gmail.com>
2025-10-19 21:22:05 +0300

    attempted lab

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,30 @@
-class Pet:
-    pass
-
 class Owner:
-    pass
+    def __init__(self, name):
+        self.name = name
+    
+    def pets(self):
+        return [pet for pet in Pet.all if pet.owner == self]
+    
+    def add_pet(self, pet):
+        if not isinstance(pet, Pet):
+            raise Exception("Must be an instance of Pet")
+        pet.owner = self
+    
+    def get_sorted_pets(self):
+        
+        owner_pets = self.pets()
+        return sorted(owner_pets, key=lambda pet: pet.name, reverse=True)  
+
+
+class Pet:
+    PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
+    all = []
+
+    def __init__(self, name, pet_type, owner=None):
+        if pet_type not in self.PET_TYPES:
+            raise Exception(f"Invalid pet type: {pet_type}. Must be one of {self.PET_TYPES}")
+        
+        self.name = name
+        self.pet_type = pet_type
+        self.owner = owner
+        self.__class__.all.append(self)
