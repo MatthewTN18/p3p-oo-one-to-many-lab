4aa3c883e57931d697adad2ed9eff6b63e4c0612
MatthewTN18 <matthewtndirangu@gmail.com>
2025-10-19 22:04:00 +0300

    completed lab

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,3 +1,16 @@
+class Pet:
+    PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
+    all = []
+
+    def __init__(self, name, pet_type, owner=None):
+        if pet_type not in self.PET_TYPES:
+            raise Exception(f"Invalid pet type: {pet_type}. Must be one of {self.PET_TYPES}")
+        
+        self.name = name
+        self.pet_type = pet_type
+        self.owner = owner
+        Pet.all.append(self)  # Use Pet.all instead of self.__class__.all
+
 class Owner:
     def __init__(self, name):
         self.name = name
@@ -11,20 +24,5 @@ class Owner:
         pet.owner = self
     
     def get_sorted_pets(self):
-        
         owner_pets = self.pets()
-        return sorted(owner_pets, key=lambda pet: pet.name, reverse=True)  
-
-
-class Pet:
-    PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
-    all = []
-
-    def __init__(self, name, pet_type, owner=None):
-        if pet_type not in self.PET_TYPES:
-            raise Exception(f"Invalid pet type: {pet_type}. Must be one of {self.PET_TYPES}")
-        
-        self.name = name
-        self.pet_type = pet_type
-        self.owner = owner
-        self.__class__.all.append(self)
+        return sorted(owner_pets, key=lambda pet: pet.name)
